16fb9b43213039ca2f9eae727509692a037e3c76
Miloš Đelić <miloss.djelic@gmail.com>
2013-09-09 01:28:34 +0200

    Button function added

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ var interval = 3000;
 var delay = 900; // offset for gray circles 
 var height;
 
-var initDivs = function(){
+var initDivs = function() {
 	numberOfChildren = $('#box').children().length;
 	height = $("#box").height();
+	createButtons();
 	rotateDiv();
 	setInterval(rotateDiv, interval);	
 	
-	};
+};
 
-var rotateDiv = function(){
+var rotateDiv = function() {
 		$('#buttons').children().css('background', 'none');
 		$('#buttons').children().eq(counter).css('background', 'darkgray');
 
@@ -23,6 +24,15 @@ var rotateDiv = function(){
 
 		counter++;
 		counter%=numberOfChildren;
+};
+
+var createButtons = function() {
+	for (var i = 0; i < numberOfChildren; i += 1) {
+	
+		var newDiv = document.createElement('div');
+		document.getElementById("buttons").appendChild(newDiv);		
+		document.getElementById("buttons").innerHTML += (" ");
 	}
+}
 
 	
